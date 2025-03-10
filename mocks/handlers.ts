96fb1cedcd6eddea37af44d34f6c66c3dd931de5
Kalyan A <kalyan.shelf@gmail.com>
2025-03-11 09:21:03 +1000

    fix notification

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -1,5 +1,6 @@
 import { authHandlers } from './handlers/auth-handlers';
 import { issuesHandlers } from './handlers/issues-handlers';
+import { notificationHandlers } from './handlers/notification-handlers';
 import { projectHandlers } from './handlers/project-handlers';
 import { sprintHandlers } from './handlers/sprint-handlers';
 import { userHandlers } from './handlers/user-handlers';
@@ -10,4 +11,5 @@ export const handlers = [
   ...projectHandlers,
   ...issuesHandlers,
   ...sprintHandlers,
+  ...notificationHandlers,
 ];
